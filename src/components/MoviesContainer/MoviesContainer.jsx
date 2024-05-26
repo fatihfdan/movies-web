b685a2e6957553581d4f39fcc6eee0a270a50a1f
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-05-27 02:48:00 +0300

    Movies Web
    save

--- src/components/MoviesContainer/MoviesContainer.jsx
+++ src/components/MoviesContainer/MoviesContainer.jsx
@@ -1,5 +1,5 @@
 import { Button } from "antd";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { GlobalContext } from "../../Context/GlobalState";
 import MovieCard from "../MovieCard/MovieCard";
 import MoviesPagination from "../MoviesPagination/MoviesPagination";
@@ -7,41 +7,26 @@ import "./moviescontainer.css";
 
 function MoviesContainer() {
   const { genres, movies } = useContext(GlobalContext);
-  const [selectedGenre, setSelectedGenre] = useState(null);
 
   const genreArray = Object.entries(genres).map(([id, name]) => ({ id, name }));
-
-  const filterMoviesByGenre = (movie) => {
-    return !selectedGenre || movie.genre_ids.includes(selectedGenre);
-  };
 
   return (
     <div className="movies-container">
       <div className="filters-button">
         {genreArray.map((genre) => (
-          <Button
-            className="genres-button"
-            key={genre.id}
-            onClick={() => setSelectedGenre(parseInt(genre.id))}
-            type={selectedGenre === parseInt(genre.id) ? "primary" : "default"}
-          >
+          <Button className="genres-button" key={genre.id}>
             {genre.name}
           </Button>
         ))}
-        <Button
-          className="genres-button"
-          onClick={() => setSelectedGenre(null)}
-          type={!selectedGenre ? "primary" : "default"}
-        >
-          Tüm Filmler
-        </Button>
       </div>
       <div className="movies-list">
-        {movies.filter(filterMoviesByGenre).map((movie) => (
+        {movies.map((movie) => (
           <MovieCard key={movie.id} id={movie.id} {...movie} />
         ))}
       </div>
-      <MoviesPagination />
+      <div className="movies-container-pagination">
+        <MoviesPagination />
+      </div>
     </div>
   );
 }

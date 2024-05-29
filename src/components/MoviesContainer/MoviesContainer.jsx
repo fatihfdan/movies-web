8171b61b433b8e7809c2c7a09315b600268db7c1
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-05-29 22:23:58 +0300

    Movies Web
    save

--- src/components/MoviesContainer/MoviesContainer.jsx
+++ src/components/MoviesContainer/MoviesContainer.jsx
@@ -1,5 +1,5 @@
 import { Button } from "antd";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GlobalContext } from "../../Context/GlobalState";
 import MovieCard from "../MovieCard/MovieCard";
 import MoviesPagination from "../MoviesPagination/MoviesPagination";
@@ -7,20 +7,38 @@ import "./moviescontainer.css";
 
 function MoviesContainer() {
   const { genres, movies } = useContext(GlobalContext);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
-  const genreArray = Object.entries(genres).map(([id, name]) => ({ id, name }));
+  const genreArray = Object.entries(genres).map(([id, name]) => ({
+    id: parseInt(id),
+    name,
+  }));
+
+  const handleGenreClick = (genreId) => {
+    setSelectedGenre(genreId === selectedGenre ? null : genreId); // Seçilen kategoriyi güncelle
+  };
+
+  const filteredMovies = selectedGenre
+    ? movies.filter((movie) => movie.genre_ids.includes(selectedGenre))
+    : movies;
 
   return (
     <div className="movies-container">
       <div className="filters-button">
         {genreArray.map((genre) => (
-          <Button className="genres-button" key={genre.id}>
+          <Button
+            className={`genres-button ${
+              selectedGenre === genre.id ? "selected" : ""
+            }`}
+            key={genre.id}
+            onClick={() => handleGenreClick(genre.id)} // Tıklama işlevi ekle
+          >
             {genre.name}
           </Button>
         ))}
       </div>
       <div className="movies-list">
-        {movies.map((movie) => (
+        {filteredMovies.map((movie) => (
           <MovieCard key={movie.id} id={movie.id} {...movie} />
         ))}
       </div>

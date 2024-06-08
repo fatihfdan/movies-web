00584a1769c97f08c1a573ae159744d58ee5f6cc
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-06-08 13:27:10 +0300

    Movies Web
    save

--- src/components/CartDetail/CartDetail.jsx
+++ src/components/CartDetail/CartDetail.jsx
@@ -44,14 +44,30 @@ function CartDetail() {
               {movie ? (
                 <div className="moviecard">
                   {movie.videos?.results.length !== 0 ? (
-                    <iframe
-                      src={`https://www.youtube.com/embed/${movie.videos.results[0].key}`}
-                      title="YouTube video player"
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                      referrerPolicy="strict-origin-when-cross-origin"
-                      allowFullScreen
-                    ></iframe>
+                    <div
+                      style={{
+                        position: "relative",
+                        paddingBottom: "56.25%",
+                        height: 0,
+                        overflow: "hidden",
+                      }}
+                    >
+                      <iframe
+                        src={`https://www.youtube.com/embed/${movie.videos.results[0].key}`}
+                        title="YouTube video player"
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen
+                        style={{
+                          position: "absolute",
+                          top: 0,
+                          left: 0,
+                          width: "100%",
+                          height: "100%",
+                        }}
+                      ></iframe>
+                    </div>
                   ) : (
                     <div className="video-placeholder">Video Bulunamadı</div>
                   )}

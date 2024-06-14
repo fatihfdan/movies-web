427ac03e1cca612ea540659c979be0e1da407494
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-06-14 18:25:12 +0300

    Movies Web
    save

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,7 @@
 // Header.jsx
 import "./header.css";
 import "../../App.css";
-import { Menu } from "antd";
-import { Input } from "antd";
+import { Menu, Input, Row, Col } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import { useContext, useState } from "react";
 import { GlobalContext } from "../../Context/GlobalState";
@@ -41,31 +40,49 @@ function Header() {
 
   return (
     <div className="header-container">
-      <Menu
-        className="movies-menu"
-        defaultSelectedKeys={["home"]}
-        mode="horizontal"
-        items={items}
-        selectedKeys={null}
-        onClick={handleMenuClick}
-      />
-      <div className="search-container">
-        <Input
-          className="movies-search"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          placeholder="Search Movies..."
-          onPressEnter={handleSearch}
-          suffix={
-            <SearchOutlined
-              onClick={handleSearch}
-              style={{ fontSize: "20px" }}
+      <Row justify="space-between" align="middle" style={{ width: "100%" }}>
+        {/* Left Section: Home and Movies */}
+        <Col xs={9} sm={12} md={6} lg={6} xl={6}>
+          <Menu
+            className="movies-menu"
+            defaultSelectedKeys={["home"]}
+            mode="horizontal"
+            items={items}
+            selectedKeys={null}
+            onClick={handleMenuClick}
+          />
+        </Col>
+
+        <Col xs={12} sm={24} md={12} lg={12} xl={12}>
+          <div className="search-container">
+            <Input
+              className="movies-search"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search Movies..."
+              onPressEnter={handleSearch}
+              suffix={
+                <SearchOutlined
+                  onClick={handleSearch}
+                  style={{ fontSize: "20px" }}
+                />
+              }
+              style={{ width: "100%", maxWidth: 400 }}
             />
-          }
-        />
-        <DarkMode />
-        <UserButton />
-      </div>
+          </div>
+        </Col>
+
+        <Col xs={24} sm={12} md={6} lg={6} xl={6}>
+          <Row justify="end" align="middle">
+            <Col>
+              <DarkMode />
+            </Col>
+            <Col style={{ marginLeft: 16 }}>
+              <UserButton />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 }

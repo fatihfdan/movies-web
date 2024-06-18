6dded23c4a5cf15df02119c0d06b883e94f5ab4c
fatihfdan <105379228+fatihfdan@users.noreply.github.com>
2024-06-19 01:36:20 +0300

    Movies Web
    fx

--- src/components/MainCard/MainCard.jsx
+++ src/components/MainCard/MainCard.jsx
@@ -1,17 +1,42 @@
-import mainCardImg from "../../assets/movie.png";
+import { useContext, useEffect, useState } from "react";
 import { Button, Space } from "antd";
 import {
   CalendarOutlined,
   HistoryOutlined,
   StarFilled,
 } from "@ant-design/icons";
+import { GlobalContext } from "../../Context/GlobalState"; // GlobalContext yolunu kendi dosya yapınıza göre ayarlayın
 import "./maincard.css";
 
 function MainCard() {
-  const img = mainCardImg;
+  const { movies, fetchData, genres, loading, currentPage } =
+    useContext(GlobalContext);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    if (movies.length > 0) {
+      setMovie(movies[1]); // Ana film olarak listeden ilk filmi alıyoruz
+    } else {
+      fetchData(currentPage);
+    }
+  }, [movies, currentPage, fetchData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!movie) {
+    return <div>No movie found</div>;
+  }
+
+  const img = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`;
+  const runtime = movie.runtime || 0;
+  const hours = Math.floor(runtime / 60);
+  const minutes = runtime % 60;
+
   return (
     <div className="maincard">
-      <img src={img} alt="img" className="maincard-img" />
+      <img src={img} alt={movie.title} className="maincard-img" />
       <div className="button-container">
         <Space>
           <Button type="primary" className="primary-button">
@@ -23,17 +48,28 @@ function MainCard() {
         </Space>
       </div>
       <div className="details-container">
-        <h1 className="movie-title">Avatar: The Way of Water</h1>
+        <h1 className="movie-title">{movie.title}</h1>
         <Space className="buttons-icons">
-          <Button className="category-button">Action</Button>
-          <Button className="category-button">Adventure</Button>
-          <Button className="category-button">Science Fiction</Button>
+          {movie.genre_ids.map((genreId) => (
+            <Button key={genreId} className="category-button">
+              {genres[genreId]}
+            </Button>
+          ))}
           <CalendarOutlined className="calendar-icon" />
-          <p className="calendar-text">2022</p>
-          <HistoryOutlined className="history-icon" />
-          <p className="history-text">3:12:00</p>
+          <p className="calendar-text">
+            {new Date(movie.release_date).getFullYear()}
+          </p>
+          {runtime > 0 && (
+            <>
+              <HistoryOutlined className="history-icon" />
+              <p className="history-text">
+                {hours}:{minutes < 10 ? "0" : ""}
+                {minutes}
+              </p>
+            </>
+          )}
           <StarFilled className="star-icon" />
-          <p className="star-text">7.5</p>
+          <p className="star-text">{movie.vote_average}</p>
         </Space>
       </div>
     </div>
